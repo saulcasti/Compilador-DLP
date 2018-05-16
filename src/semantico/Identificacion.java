ea84d4c6396eb496d0a39574ff13576838e59916
saulcasti <saul_casti@hotmail.es>
2018-05-16 21:11:39 +0200

    Mejora de javadoc

--- src/semantico/Identificacion.java
+++ src/semantico/Identificacion.java
@@ -14,6 +14,8 @@ import visitor.*;
 public class Identificacion extends DefaultVisitor {
 
 	private GestorErrores gestorErrores;
+	
+	//Conjuntos auxiliares
 	private Map<String, DefFuncion> funciones = new HashMap<String, DefFuncion>();
 	private ContextMap<String, DefVariable> variables = new ContextMap<String, DefVariable>();
 	private Map<String, DefEstructura> estructuras = new HashMap<String, DefEstructura>();
@@ -23,8 +25,15 @@ public class Identificacion extends DefaultVisitor {
 	}
 
 	
-	
-//	class DefVariable { Tipo tipo;  String nombre; }
+	/**
+	 * Nodo DefVariable
+	 * 
+	 * defVariable:definicion -> nombre:String tipo:tipo ambito:int
+	 * 			Predicado: 			variables.buscarActual(nombre) == null
+	 * 			Reglas Semánticas: 	variables[nombre] = defVariable
+	 * 
+	 * class DefVariable { Tipo tipo;  String nombre; }
+	 */
 	public Object visit(DefVariable node, Object param) {
 		
 		DefVariable definicion = variables.getFromTop(node.getNombre());
@@ -34,18 +43,23 @@ public class Identificacion extends DefaultVisitor {
 	}
 
 	
-//	class Variable { String nombre; }
-	public Object visit(Variable node, Object param) {
-		DefVariable definicion = variables.getFromAny(node.getNombre());
-		predicado(definicion != null,"Variable no definida: " + node.getNombre(), node.getStart());
-		node.setDefinicion(definicion); // Enlazar referencia con definición	
-		return null;
-	}
-
-
-	
-//	class DefFuncion { String nombre;  List<DefParametro> parametros;  Retorno retorno;  Cuerpo cuerpo; }
-	public Object visit(DefFuncion node, Object param) {
+	/**
+	 * Nodo DefFuncion
+	 * 
+	 * defFuncion:definicion -> nombre:String parametros:defVariable* retorno:retorno cuerpo:cuerpo
+	 * 		Predicado:			funciones[nombre] == null
+	 * 		Reglas Semánticas:	funciones[nombre]=defFuncion
+	 * 											{
+	 * 												variables.set()
+	 * 												visit(parámetros i)
+	 * 												visit(cuerpo, funciones[nombre])
+	 * 												variable.reset()
+	 * 											}
+	 * 
+	 * 
+	 * class DefFuncion { String nombre;  List<DefParametro> parametros;  Retorno retorno;  Cuerpo cuerpo; }
+	 */
+public Object visit(DefFuncion node, Object param) {
 		DefFuncion definicion = funciones.get(node.getNombre());
 		predicado(definicion == null, "Función ya definida: " + node.getNombre(), node.getStart());
 		funciones.put(node.getNombre(), node);
@@ -56,37 +70,27 @@ public class Identificacion extends DefaultVisitor {
 		
 		return null;
 	}
-//	class Return { Expresion expresion; }
-	public Object visit(Return node, Object param) {
-		node.setFuncion((DefFuncion) param);
-		return super.visit(node, param);
-	}
-	
-	//	class Invocacion { String nombre;  List<Expresion> argumentos; }
-	public Object visit(Invocacion node, Object param) {
-		DefFuncion definicion = funciones.get(node.getNombre());
-		predicado(definicion != null,"Funcion no definida: " + node.getNombre(), node.getStart());
-		
-		node.setDefFuncion(definicion); // Enlazar referencia con definición
-
-		return super.visit(node, param);
-	}
-
-	//	class LlamadaFuncionSentencia { String nombre;  List<Expresion> argumentos; }
-	public Object visit(LlamadaFuncionSentencia node, Object param) {
-		DefFuncion definicion = funciones.get(node.getNombre());
-		predicado(definicion != null,"Funcion no definida: " + node.getNombre(), node.getStart());
-
-		node.setDefFuncion(definicion); // Enlazar referencia con definición
-		return super.visit(node, param);
-	}
 	
 	
-	//	class DefEstructura { String nombre;  List<DefCampo> defcampo; }
+
+	/**
+	 * Nodo DefEstructura
+	 * 
+	 * defEstructura:definicion -> nombre:String defcampo:defCampo*
+	 * 			Predicado: 			estructuras[nombre] == null
+	 * 			Reglas Semánticas: 	estructuras[nombre]=defEstructura
+	 *												{
+	 *												    visit(defCampo i)
+	 *												}
+	 * 
+	 * class DefEstructura { String nombre;  List<DefCampo> defcampo; }
+	 */
 	public Object visit(DefEstructura node, Object param) {
 		DefEstructura definicion = estructuras.get(node.getNombre());
 		predicado(definicion == null, "Estructura ya definida: " + node.getNombre(), node.getStart());
 		
+	
+		// DefCampo--> Predicado: campos[nombre] == null | Reglas Semánticas: campos[nombre] = defCamp
 		Map<String, DefCampo> campos = new HashMap<String, DefCampo>();
 		for(DefCampo campo:node.getDefcampo()) {
 			DefCampo defCampo = campos.get(campo.getNombre());
@@ -101,26 +105,90 @@ public class Identificacion extends DefaultVisitor {
 	}
 	
 	
-	//	class IdentType { String nombre; }
+	/**
+	 * Nodo IdentType
+	 * 
+	 * identType:tipo ->nombre:String
+	 * 		Predicado:			estructuras[nombre] != null
+	 * 		Reglas Semánticas:	identType.definicion = estructuras[nombre]
+	 * 
+	 * class IdentType { String nombre; }
+	 */
 	public Object visit(IdentType node, Object param) {
 		DefEstructura definicion = estructuras.get(node.getNombre());
 		predicado(definicion != null,"Estructura no definida: " + node.getNombre(), node.getStart());
 		
-		node.setDefinicion(definicion); // Enlazar referencia con definición
+		node.setDefinicion(definicion);
 		return super.visit(node, param);
 	}
 	
-	/*
-	 * Poner aquí los visit necesarios.
-	 * Si se ha usado VGen solo hay que copiarlos de la clase 'visitor/_PlantillaParaVisitors.txt'.
+	/**
+	 * Nodo Return
+	 * 
+	 * return:sentencia -> expresion:expresion
+	 * 		Regla Semática:	return.definicion = defFuncion
+	 * 
+	 * class Return { Expresion expresion; }
 	 */
-
-	// public Object visit(Programa prog, Object param) {
-	// ...
-	// }
-
+	public Object visit(Return node, Object param) {
+		node.setFuncion((DefFuncion) param);
+		return super.visit(node, param);
+	}
 	
 	
+	/**
+	 * Nodo LlamadaFuncionSentencia
+	 * 
+	 * llamadaFuncionSentencia:sentencia -> nombre:String argumentos:expresion*
+	 * 		Predicado:			funciones[nombre] != null
+	 * 		Regla Semántica:	llamadaFuncionSentencia.definicion = funciones[nombre]
+	 * 
+	 * class LlamadaFuncionSentencia { String nombre;  List<Expresion> argumentos; }
+	 */
+	public Object visit(LlamadaFuncionSentencia node, Object param) {
+		DefFuncion definicion = funciones.get(node.getNombre());
+		predicado(definicion != null,"Funcion no definida: " + node.getNombre(), node.getStart());
+
+		node.setDefFuncion(definicion); 
+		return super.visit(node, param);
+	}
+	
+
+	/**
+	 * Nodo Invocacion
+	 * 
+	 * invocacion:expresion -> nombre:String argumentos:expresion*
+	 * 		Predicado:			funciones[nombre] != null
+	 * 		Regla Semántica:	invocacion.definicion = funciones[nombre]
+	 * 
+	 * class Invocacion { String nombre;  List<Expresion> argumentos; }
+	 */
+	public Object visit(Invocacion node, Object param) {
+		DefFuncion definicion = funciones.get(node.getNombre());
+		predicado(definicion != null,"Funcion no definida: " + node.getNombre(), node.getStart());
+		
+		node.setDefFuncion(definicion); // Enlazar referencia con definición
+
+		return super.visit(node, param);
+	}
+	
+	
+	/**
+	 * Nodo Variable
+	 * 
+	 * variable:expresion -> nombre:String
+	 * 		Predicado:			variables.buscar(nombre) != null
+	 * 		Regla Semántica:	variable.definición = variablesbuscar(nombre)
+	 * 
+	 * class Variable { String nombre; }
+	 */
+	public Object visit(Variable node, Object param) {
+		DefVariable definicion = variables.getFromAny(node.getNombre());
+		predicado(definicion != null,"Variable no definida: " + node.getNombre(), node.getStart());
+		node.setDefinicion(definicion); // Enlazar referencia con definición	
+		return null;
+	}
+
 	
 	
 	/**
